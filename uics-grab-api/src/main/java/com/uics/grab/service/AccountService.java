cb0e3e98bdb758e88e4f8aafe72f396acc90e98c
tom <tom@daoshenggroup.com>
2016-12-07 10:36:46 +0800

    init

---
+++ uics-grab-api/src/main/java/com/uics/grab/service/AccountService.java
@@ -0,0 +1,10 @@
+package com.uics.grab.service;
+
+import com.uics.grab.entity.Account;
+
+/**
+ * @author baiqw
+ */
+public interface AccountService {
+    Account signIn(Account account);
+}
---
+++ uics-grab-service-provider/src/main/java/com/uics/grab/service/AccountServiceImpl.java
@@ -0,0 +1,13 @@
+package com.uics.grab.service;
+
+import com.uics.grab.entity.Account;
+
+/**
+ * @author baiqw
+ */
+public class AccountServiceImpl implements AccountService {
+    @Override
+    public Account signIn(Account account) {
+        return null;
+    }
+}
