ca07d269dac10528f234d9c0e336726c203e76f1
tom <tom@daoshenggroup.com>
2016-12-13 10:23:52 +0800

    init vrv config

---
+++ uics-grab-service-provider/src/test/resources/data/postgres/import-data.sql
@@ -0,0 +1,2 @@
+insert into t_vrv_config (id, ip, name, cycle, last_refresh_date) values ('1','192.168.1.1','吉安', 10000,0);
+insert into t_vrv_config (id, ip, name, cycle, last_refresh_date) values ('2','192.168.1.2','九江', 10000,0);
